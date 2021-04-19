e24d8858111c4f633f5bed00206facc41045892b
PhiliciaWatson <philiciawatson7@gmail.com>
2021-04-19 07:20:20 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,54 @@
+var Sunshine = 70;
+var Cake = 23;
+
+function addSum(arg1,arg2){
+    return arg1 + arg2;
+}
+
+console.log(addSum(Sunshine,Cake))
+
+function Sub(arg1,arg2){
+    return arg1 - arg2;
+}
+
+console.log(Sub(Sunshine,Cake))
+
+function Multiply(arg1,arg2){
+    return arg1 * arg2;
+}
+
+console.log(Multiply(Sunshine,Cake))
+
+function Divide(arg1,arg2){
+    return arg1 / arg2;
+}
+
+console.log(Divide(Sunshine,Cake))
+
+addSum = (arg1,arg2) => {
+    return arg1 + arg2;
+}
+
+console.log(addSum(Sunshine,Cake))
+
+Sub = (arg1,arg2) => {
+    return arg1 - arg2;
+}
+
+console.log(Sub(Sunshine,Cake))
+
+
+// arrow functions below
+
+
+Multiply = (arg1,arg2) => {
+    return arg1 * arg2;
+}
+
+console.log(Multiply(Sunshine,Cake))
+
+Divide = (arg1,arg2) => {
+    return arg1 / arg2;
+}
+
+console.log(Divide(Sunshine,Cake))
